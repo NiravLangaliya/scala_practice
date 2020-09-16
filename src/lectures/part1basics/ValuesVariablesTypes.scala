7d7a6a448facff5da29b14f6ff43f84b7631d50a
Nirav Langaliya <nirav.langaliya.externo@cmpc.cl>
2020-09-16 23:14:17 +0530

    Added lecture#1

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,11 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App{
+  //val are immutable, val act same as constance or final in Java
+
+ val x : Int = 42
+  println(x)
+  //type of val is optional as complier can infer type of val from checking value assinged to it.
+  val y = 50
+  println(y)
+}
