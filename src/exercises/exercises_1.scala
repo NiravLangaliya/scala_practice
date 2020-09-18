fb64cbf33f3413029d7e9f4d82e043492b389ac1
Nirav Langaliya <nirav.langaliya.externo@cmpc.cl>
2020-09-19 00:16:59 +0530

    Added lecture#1

--- src/exercises/exercises_1.scala
+++ src/exercises/exercises_1.scala
@@ -4,6 +4,10 @@ object exercises_1 extends App {
 	
 	//1 differance between "Hello World" and println("Hello World")
 	
+	//"Hello World" ---> this is value with string type
+	//println("Hello World") ---> this is expression and which has side effect of printing.
+	
+	
 	//2 what would be type for below val
 	val someValue = {
 		2 < 3
@@ -11,6 +15,7 @@ object exercises_1 extends App {
 	
 	println(someValue)
 	
+	//3
 	val someotherVaule = {
 		if (someValue) 2 else 3
 		45
