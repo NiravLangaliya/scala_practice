d26cb74295975c543a8500dc7cf91ed4afc12600
Nirav Langaliya <nirav.langaliya.externo@cmpc.cl>
2020-09-21 21:21:44 +0530

    Added lecture#1

---
+++ src/exercises/Recursion.scala
@@ -0,0 +1,54 @@
+package exercises
+
+import scala.annotation.tailrec
+
+object Recursion extends App {
+	
+	// 1. Concatenate a String n times.
+	// 2. IsPrime Function tail recursive
+	// 3. Fibonacci Function , tail recursive
+	
+	// 1. Concatenate a String n times.
+	@tailrec
+	def concatenateTailrec(aString: String, n: Int, accumulator: String): String
+	= {
+		if (n <= 0) accumulator
+		else concatenateTailrec(aString, n - 1: Int, aString + accumulator)
+	}
+	
+	println(concatenateTailrec("Hello ", 5, "How are you"))
+	
+	// 2. IsPrime Function tail recursive
+	
+	def functionIsPrimt(n: Int): Boolean = {
+		@tailrec
+		def isPrimeTailrec(t: Int, isStillPrime: Boolean): Boolean = {
+			if (!isStillPrime) false
+			else if (t <= 1) true
+			else isPrimeTailrec(t - 1, n % t != 0 && isStillPrime)
+		}
+		
+		isPrimeTailrec(n / 2, true)
+	}
+	
+	println(functionIsPrimt(629))
+	println(functionIsPrimt(1000))
+	println(functionIsPrimt(2003))
+	
+	// 3. Fibonacci Function , tail recursive
+	
+	def aFibonaccinumber(n: Int): Int = {
+		@tailrec
+		def fiboTailrec(i: Int, last: Int, nextToLast: Int): Int =
+			if (i >= n) last
+			else fiboTailrec(i + 1, last + nextToLast, last)
+		
+		if (n <= 2) 1
+		else fiboTailrec(2, 1, 1)
+	}
+	
+	println(aFibonaccinumber(8))
+	// 1 1 2 3 5 8 13 21
+	
+}
+
--- src/lectures/part1basics/Recursion.scala
+++ src/lectures/part1basics/Recursion.scala
@@ -1,5 +1,7 @@
 package lectures.part1basics
 
+import scala.annotation.tailrec
+
 object Recursion extends App {
 	
 	def factorial(n: Int): Int = {
@@ -8,5 +10,38 @@ object Recursion extends App {
 	}
 	
 	println(factorial(4))
+	
+	
+	// call stack
+	def factorial_type2(n: Int): Int =
+		if (n <= 1) 1
+		else {
+			println("Computing factorial of" + n + " - I frist need factorial of " + (n - 1))
+			val result = n * factorial_type2(n - 1)
+			println("computed factorial of " + (n))
+			result
+		}
+	
+	//factorial_type2(50000)
+	
+	def factorica_type3(n: Int): BigInt = {
+		@tailrec
+		def factHelper(x: Int, accumulator: BigInt): BigInt = {
+			if (x <= 1) accumulator
+			else factHelper(x - 1, x * accumulator)
+			// TAil Recursion = use recursion call as then last expression
+			
+		}
+		
+		factHelper(n, 1)
+	}
+	
+	println(factorica_type3(50000))
+	/*
+	factorica_type3(10) = factHelper(10,1)
+	= factHelper(9,10*1)
+	= factHelper(8,9*10*1)
+	 */
+	// when you need loops , use tail recurision
 	
 }
